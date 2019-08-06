7757d10f4f5f7d60901b6734f0bb4b727aa40da8
sabquat <32111842+sabquat@users.noreply.github.com>
2019-08-06 16:55:46 -0500

    Update ordercitations.py

--- ordercitations.py
+++ ordercitations.py
@@ -11,28 +11,26 @@ if len(sys.argv) < 2:
 else:
   file = sys.argv[1]
 
-started = False
-items = {}
-for line in open(file,"r"):
-    if line.startswith('\\end{thebibliography}'):
-        started = False
-    if started:
-        if line.startswith('\\bibitem'):
-            name = re.findall( 'bibitem{(.*?)\}', line )[0].strip(' \t\n\r')
-            items[name] = line
-            print name
-
-    if not started and line.startswith('\\begin{thebibliography}'):
-        started = True
-
 cites = []
+count = {}
+emptyCount = 0
 for line in open(file,"r"):
     for m in re.findall( 'cite{(.*?)\}', line ):
         for cite in  m.split(","):
-
-            if not cite.strip(' \t\n\r') in cites:
-                cites.append(cite.strip(' \t\n\r'))
+            temp = cite.strip(' \t\n\r');
+            if not temp:
+                emptyCount +=1;
+                continue
+            if not temp in cites:
+                cites.append(temp)
+                count[temp] = 1
+            else:
+                count[temp] += 1
 
 for c in cites:
-    print items[c]
+    print("%s %d" % (c, count[c]))
 
+CRED = '\033[91m'
+CEND = '\033[0m'
+if emptyCount:
+    print (CRED + "warning: " + str(emptyCount) + " empty cite{}" + CEND)
